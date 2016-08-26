90c940bd2d451aed1953347adb740e99681e1dec
Subhajit Sahu <wolfram77@gmail.com>
2016-08-26 13:09:11 +0530

    project: created and updated cmd-module->src

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+// install[as]
+// create[as]
+// remove
+// rename
+// list
+// help
+namespace orez.omodule {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
